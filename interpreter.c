9343f31f95fa539fbe10142e8ee7bb02430fa6cb
Bernardo De Azevedo Moreira <bernardo.az.moreira@gmail.com>
2017-05-31 15:57:05 -0300

    Pequenos ajustes e laço de leitura adicionado

--- interpreter.c
+++ interpreter.c
@@ -1,13 +1,22 @@
 #include <stdio.h> 
 #include <stdlib.h>
-
-/* memory space for pointer */
-unsigned char mem[30000] = {0};
-/* the pointer itself */
-unsigned char* ptr = mem;
+#include <string.h>
 
 void interpret(char* in)
 {
+    unsigned char mem[30000] = {0}; /* memory space for pointer */
+    unsigned char* ptr = mem; /* the pointer itself */
+    
+    char read;      /* char being read */
+
+    size_t i;       /* size_t is apparently more adequate to */
+    size_t loop;    /* control unsigned arrays and pointers  */
+
+    for (i = 0; in[i] != '\0' && in[i] != EOF; i++)
+    {
+        
+    }
+
     return;
 }
 
