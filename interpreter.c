77df9214936e5dbd2f369780e8d01bb292043d27
Bernardo De Azevedo Moreira <bernardo.az.moreira@gmail.com>
2017-05-31 15:45:57 -0300

    Esqueleto de Código Adicionado

--- interpreter.c
+++ interpreter.c
@@ -1,1 +1,17 @@
- 
+#include <stdio.h> 
+#include <stdlib.h>
+
+/* memory space for pointer */
+unsigned char mem[30000] = {0};
+/* the pointer itself */
+unsigned char* ptr = mem;
+
+void interpret(char* in)
+{
+    return;
+}
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
